5e54391b2ab0853e57405be6c61c96cbf46a4497
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-09-16 11:46:19 -0600

    AsyncCommands removed

--- chapter_03/src/models/redis_model.rs
+++ chapter_03/src/models/redis_model.rs
@@ -1,8 +1,5 @@
 use crate::errors::AppError;
-use deadpool_redis::{
-    Pool,
-    redis::{AsyncCommands, AsyncTypedCommands},
-};
+use deadpool_redis::{Pool, redis::AsyncTypedCommands};
 
 // Guardar valor en Redis
 pub async fn set_value(pool: &Pool, key: &str, value: &str) -> Result<(), AppError> {
@@ -16,7 +13,7 @@ pub async fn set_value(pool: &Pool, key: &str, value: &str) -> Result<(), AppErr
 // Obtener valor de Redis
 pub async fn get_value(pool: &Pool, key: &str) -> Result<Option<String>, AppError> {
     let mut conn = pool.get().await.map_err(|_| AppError::InternalError)?;
-    let value: Option<String> = AsyncCommands::get(&mut conn, key)
+    let value: Option<String> = AsyncTypedCommands::get(&mut conn, key)
         .await
         .map_err(|_| AppError::InternalError)?;
     Ok(value)
